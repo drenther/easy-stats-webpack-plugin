f876236e4525f4d0d3a655b8ef0e50be0f139013
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-03-17 02:24:29 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		node: true,
+		commonjs: true,
+		es6: true,
+	},
+	extends: 'eslint:recommended',
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'no-console': ['error', { allow: ['log'] }],
+	},
+};
